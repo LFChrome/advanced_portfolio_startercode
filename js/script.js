1f3616f9b68a94e3ab3bfa38a901fc3700767fbe
Wei Wu Lu <wuwj2001@gmail.com>
2017-11-02 00:18:04 +0000

    jquery

--- js/script.js
+++ js/script.js
@@ -14,8 +14,10 @@
 // *************************** YOUR CODE BELOW *******************************
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
-
-
+$(document).ready(
+var height = $('.navbar').height();
+$('#block').css('height', height);
+);
 // Create your potrfolio object representation below
 
 
